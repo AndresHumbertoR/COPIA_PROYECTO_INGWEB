e72c5132c636c8d20f5639d2adde5496c5baeda1
AndresHumbertoR <85975162+AndresHumbertoR@users.noreply.github.com>
2025-10-30 00:34:50 -0500

    Add files via upload

---
+++ src/php/conexion_bd.php
@@ -0,0 +1,9 @@
+<?php
+    $conexion=mysqli_connect("localhost","id21441015_risaza01","#5A1JK4YHA7rg","id21441015_fhdb");
+
+    if($conexion){
+        // echo "Conexion a la base de datos exitosa!";
+    }else{
+        die("La conexion a la base de datos ha fallado!");
+    }
+?>
